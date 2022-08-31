de07dc881725afbf47b7acfb901e6111663e03d5
Minskoleopc <minskoleopc@gmail.com>
2022-08-31 11:42:05 +0530

    added

---
+++ DOM5/script2.js
@@ -0,0 +1,51 @@
+//<h1 class = "one" id ="two" name ="nm">Heading1</h1>
+// id  
+let byId = document.querySelector('#two')
+// className 
+let byClassName = document.querySelector('.one')
+// tagName 
+let headOne = document.querySelector('h1')
+// commonF  // tagName[attibute="value"]
+let comF = document.querySelector('h1[name ="nm"]')
+console.log(byId)
+console.log(byClassName)
+console.log(headOne)
+console.log(comF)
+
+//console.log(byId.textContent)
+//byId.textContent = "Poorva"
+
+//program 1
+// byId.addEventListener('click',function(){
+//     byId.textContent = "Poorva"
+// })
+
+
+// program2
+
+let headTwo = document.querySelector('h2')
+console.log(headTwo)
+headTwo.addEventListener('click',function(){
+    headTwo.style.color = "green"
+    headTwo.style.background = "yellow"
+})
+
+
+// program 3
+let headThree = document.querySelector('h3')
+let buttonA = document.querySelector('button')
+
+buttonA.addEventListener('click',function(){
+    headThree.textContent = "chinmay"
+    headThree.style.color = "purple"
+    headThree.style.background = "black"
+})
+
+
+// let info = {
+//     parent:{
+//         mother:"kanchan"
+//     }
+// }
+
+// info.parent.mother = "kanchan s"
